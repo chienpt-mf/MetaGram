1e97b65138b8ef5869102dcb090c552a00ba7da8
chienpt-mf <pham.thi.chien@moneyforward.co.jp>
2021-11-08 22:31:36 +0700

    create post controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   devise_for :users
+  resources :posts, only: [:index, :show, :create, :destroy] do
+    resources :photos, only: [:create]
+  end
 end
