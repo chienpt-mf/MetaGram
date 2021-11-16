77ef63841cb93b3388a6eda38023b724cbea169b
chienpt-mf <pham.thi.chien@moneyforward.co.jp>
2021-11-16 16:50:46 +0700

    add comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     before_action :find_post, only: [:show, :destroy]
   
     def index
-      @posts =  Post.eager_load(:photos, :user, likes: :user).recent_post.limit 10
+      @posts = Post.eager_load(:photos, :user, likes: :user, comments: :user).recent_post.limit 10
       @post = Post.new
     end
   
@@ -27,6 +27,7 @@ class PostsController < ApplicationController
     def show
       @photos = @post.photos
       @likes = @post.likes.includes :user
+      @comment = Comment.new
       @is_likes = @post.is_liked? current_user
     end
   
