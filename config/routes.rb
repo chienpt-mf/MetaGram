ea9b0e7d77bf9a8634338407487dffcb97560ee1
chienpt-mf <pham.thi.chien@moneyforward.co.jp>
2021-11-30 18:26:29 +0700

    add edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   resources :users, only: [:index, :show] do
     member do
